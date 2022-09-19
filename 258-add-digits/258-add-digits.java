a02444a5d08ef1a6c94cf5f7c034140d1375e3c4
YashGangwar <80022075+btw-itz-Yash@users.noreply.github.com>
2022-09-19 20:58:01 +0530

    Time: 2 ms (74.48%), Space: 42 MB (12.36%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,12 @@
+class Solution {
+      public int addDigits(int num) {
+       int sum = 0;
+            while (num > 0) {
+                int reminder = num % 10;
+                num = num / 10;
+                sum += reminder;
+            }
+      return sum<10?sum:addDigits(sum);
+     
+    }
+}
